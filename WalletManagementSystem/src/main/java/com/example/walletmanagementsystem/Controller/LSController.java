a1af1b0e101c894a90ff4d9e11234d4f45e29700
Noman Talib <nomansitalib@gmail.com>
2025-06-01 21:50:25 +0500

    i made login.fxml

--- WalletManagementSystem/src/main/java/com/example/walletmanagementsystem/Controller/LSController.java
+++ WalletManagementSystem/src/main/java/com/example/walletmanagementsystem/Controller/LSController.java
@@ -1,6 +1,7 @@
 package com.example.walletmanagementsystem.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.AnchorPane;
 
@@ -18,6 +19,16 @@ public class LSController {
 
     @FXML
     private Hyperlink Signup;
+    @FXML
+    private ComboBox<String> roleComboBox;
+
+    @FXML
+    public void initialize() {
+
+        roleComboBox.getItems().addAll("User", "Admin");
+
+        roleComboBox.setValue("User");
+    }
 
     public void switchFrom(javafx.event.ActionEvent actionEvent) {
         if (actionEvent.getSource()==Login){
